0ff766dacfb7e6ab1aed0b8eb466da58e3fc9e24
Gabri <gmacias96@gmail.com>
2021-02-05 10:01:12 +0100

    05/02

---
+++ MvcCore/Extensions/TempDataExtension.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcCorePaco.Extensions
+{
+    public static class TempDataExtension
+    {
+        public static void SetObject(this ITempDataDictionary tempdata, string key, object data)
+        {
+            tempdata[key] = JsonConvert.SerializeObject(data);
+        }
+        public static T GetObject<T>(this ITempDataDictionary tempdata, string key)
+        {
+            if (tempdata[key] != null)
+            {
+                string data = tempdata[key].ToString();
+                return JsonConvert.DeserializeObject<T>(data);
+            }
+            else { return default(T); }
+        }
+    }
+}
